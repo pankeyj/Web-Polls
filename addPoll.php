fee38c6d125e06d42da73e9f6da8d46d79a3a294
pankeyj <pankeyj@mail.gvsu.edu>
2017-02-08 13:53:18 -0500

    PHP script works. Switching to AJAX form submission

--- addPoll.php
+++ addPoll.php
@@ -2,44 +2,47 @@
 $conn = new mysqli('cis.gvsu.edu', 'pankeyj', 'pankeyj8613', 'pankeyj');
 if($conn->connect_errno)
 {
-	echo "Connection Error";
+	echo "Connection Error>";
 }
 else
 {
-	echo "connected successfully";
+	echo "Connected succesfully";
 }
+
 $title = $_POST["title"];
 $option1 = $_POST["option1"];
 $option2 = $_POST["option2"];
 $option3 = $_POST["option3"];
 $option4 = $_POST["option4"];
 
-if !empty($option3) && !empty($option4)
+if (!empty($option3) && !empty($option4))
 {
 	$sql = "INSERT INTO Polls
 	(pollName, option1, option2, option3, option4)
 	VALUES
-	('$title', '$option1', '$option3', '$option4')";
+	('$title', '$option1','$option2','$option3','$option4')";
 }
-elseif !empty($option3) && empty($option4)
+
+elseif (!empty($option3) && empty($option4))
 {
         $sql = "INSERT INTO Polls
-        (pollName, option1, option2, option3, option4)
+        (pollName, option1, option2, option3)
         VALUES
-        ('$title', '$option1', '$option3', '$option4')";
+        ('$title', '$option1','$option2','$option3')";
 }
-elseif empty($option3) && empty($option4)
+elseif (empty($option3) && empty($option4))
 {
         $sql = "INSERT INTO Polls
-        (pollName, option1, option2, option3, option4)
+        (pollName, option1, option2)
         VALUES
-        ('$title', '$option1', '$option3', '$option4')";
+        ('$title','$option1','$option2')";
 }
 else
 {
 	echo "Unable to add poll <br>";
 }
 
+echo "Code " . $sql;
 if($conn->query($sql) == TRUE)
 {
 	echo "Entry Updated <br>";
@@ -49,4 +52,5 @@ else
 {
 	echo "Error updating the table<br>";
 }
+
 ?>
